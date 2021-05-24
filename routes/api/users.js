363c0137533abf3673ebc8771110b0ae13252f7b
iCode-Cat <devranboyac8@gmail.com>
2021-05-24 23:12:50 +0300

    User register added

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const User = require('../../model/User');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const {
     check,
     validationResult
-} = require('express-validator/check')
+} = require('express-validator')
 
 //@route POST api/users
 //@ desc Test route
@@ -14,12 +19,60 @@ router.post('/', [
     .isEmail(),
     check('email', 'Please include a valid email').isEmail(),
     check('password', 'Password must be 6 or more characters').isLength({min:6})
-], (req, res) => {
+], async(req, res) => {
     const errors = validationResult(req)
     if(!errors.isEmpty()){
         return res.status(400).json({errors:errors.array()})
     }
-    res.send('users api')
+    
+    const {name, email, password} = req.body
+
+    try {
+        let user = await User.findOne({email})
+        if(user) {
+            return res.status(400).json({errors:[{msg:'This email already exists'}]})
+        }
+
+        //Avatar generate
+        const avatar = gravatar.url(email, {
+            s:200,
+            r:'pg',
+            d:'mm'
+        })
+
+        //Create user intance
+        user = new User({
+            name,
+            email,
+            avatar,
+            password
+        })
+
+        //Bcrypt
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+        
+        await user.save()
+        
+        const payload = {
+            user:{
+                id:user.id
+            }
+        };
+
+        jwt.sign(
+            payload, 
+            config.get('jwtSecret'),
+            { expiresIn: 36000 }, 
+            (err, token) => {
+                if(err) throw err;
+                res.json({token})
+            });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({errors:'Server Error'})
+    }
+
 })
 
 module.exports = router;
