908e75ebab8387d0c253f114ce87be0680cf7a3e
iCode-Cat <devranboyac8@gmail.com>
2021-06-06 23:55:22 +0300

    Redux setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,32 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+import Alert from './components/layout/Alert';
 
 const App = () => {
-  return (
+ return (
+  <Provider store={store}>
+  <Router>
     <>
-      <Navbar />
-      <Landing />
-    </>
-  );
+    <Navbar />
+    <Route exact path='/' component={Landing} />
+    <section className="container">
+    <Alert/>
+    <Switch>
+    <Route exact path='/register' component={Register} />
+    <Route exact path='/login' component={Login} />
+    </Switch>
+    </section>
+  </>
+  </Router>
+  </Provider>
+ )
 }
 
 export default App;
---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,54 @@
+
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom'
+
+const Login = () => {
+    const [ formData, setFormData ] = useState({
+        email: '',
+        password: ''
+    })
+
+    const { email, password } = formData;
+
+    const onChange = e => setFormData({ ...formData, [e.target.name]:e.target.value })
+
+    const onSubmit = async e => {
+        e.preventDefault();
+
+        console.log('SUCCESS');
+       
+    }
+
+    return (
+        <>
+            <h1 className="large text-primary">Sign In</h1>
+      <p className="lead"><i className="fas fa-user"></i> Sign Into Your Account</p>
+      <form className="form" onSubmit={onSubmit} >
+        <div className="form-group">
+          <input type="email" placeholder="Email Address" value={email} onChange={e => onChange(e)} name="email" required />
+          <small className="form-text"
+            >This site uses Gravatar so if you want a profile image, use a
+            Gravatar email</small
+          >
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            value={password}
+            onChange={e => onChange(e)}
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            required
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Don't have an account?<Link to="/register">Sign Up</Link>
+      </p>
+        </>
+    )
+}
+
+export default Login
